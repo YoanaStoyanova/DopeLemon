e52433720922dd2e8e597cdf6e5b4d21bfaef1d8
Dobromir Ivanov <dobromir0ivanov@gmail.com>
2021-01-03 20:28:58 +0200

    Use spaces instead of tabs. Ignore IDEA files.

--- src/main/java/org/playground/dopelemon/DopelemonApplication.java
+++ src/main/java/org/playground/dopelemon/DopelemonApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DopelemonApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DopelemonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DopelemonApplication.class, args);
+    }
 
 }
