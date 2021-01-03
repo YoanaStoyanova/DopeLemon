4d19cc1cdf17e7ba6835cf0f686ca316f113e6fa
Dobromir Ivanov <dobromir0ivanov@gmail.com>
2021-01-03 22:05:48 +0200

    Configure PSQL database access.

--- src/main/java/org/playground/dopelemon/DopelemonApplication.java
+++ src/main/java/org/playground/dopelemon/DopelemonApplication.java
@@ -2,7 +2,11 @@ package org.playground.dopelemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
+@Configuration
+@EnableJdbcRepositories(basePackages = "org.playground.dopelemon.repository")
 @SpringBootApplication
 public class DopelemonApplication {
     public static void main(String[] args) {
