9e613b16423902858568b6a8f58b77dae25a482d
Dobromir Ivanov <dobromir0ivanov@gmail.com>
2021-01-03 19:49:35 +0200

    Initial commit

---
+++ src/main/java/org/playground/dopelemon/DopelemonApplication.java
@@ -0,0 +1,13 @@
+package org.playground.dopelemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopelemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DopelemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/playground/dopelemon/DopelemonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.playground.dopelemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopelemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
